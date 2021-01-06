a8ceb72bdd938ebac9fd822212363688a4c5a631
CPMSP <cpetersonmsp@gmail.com>
2021-01-06 10:48:12 -0600

    Dad Jokes complete

---
+++ src/Joke.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "./Joke.css";
+
+class Joke extends Component {
+	getColor() {
+		if (this.props.votes >= 25) {
+			return "#4caf50";
+		} else if (this.props.votes >= 20) {
+			return "#8bc34a";
+		} else if (this.props.votes >= 15) {
+			return "#cddc39";
+		} else if (this.props.votes >= 10) {
+			return "#ffeb3b";
+		} else if (this.props.votes >= 5) {
+			return "#ffc107";
+		} else if (this.props.votes >= 0) {
+			return "#ff9800";
+		} else {
+			return "#f44336";
+		}
+	}
+
+	getEmoji() {
+		if (this.props.votes >= 25) {
+			return "em em-rolling_on_the_floor_laughing";
+		} else if (this.props.votes >= 20) {
+			return "em em-laughing";
+		} else if (this.props.votes >= 15) {
+			return "em em-smiley";
+		} else if (this.props.votes >= 10) {
+			return "em em-slightly_smiling_face";
+		} else if (this.props.votes >= 5) {
+			return "em em-neutral_face";
+		} else if (this.props.votes >= 0) {
+			return "em em-confused";
+		} else {
+			return "em em-angry";
+		}
+	}
+
+	render() {
+		return (
+			<div className="Joke">
+				<div className="Joke-buttons">
+					<i
+						className="fas fa-arrow-up"
+						onClick={this.props.upvote}
+					/>
+					<span
+						style={{ borderColor: this.getColor() }}
+						className="Joke-votes">
+						{this.props.votes}
+					</span>
+					<i
+						className="fas fa-arrow-down"
+						onClick={this.props.downvote}
+					/>
+				</div>
+				<div className="Joke-text">{this.props.text}</div>
+				<div className="Joke-smiley">
+					<i
+						className={this.getEmoji()}
+						aria-label="ROLLING ON THE FLOOR LAUGHING"
+					/>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Joke;
