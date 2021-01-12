a07bf7eee73410898b8e937b706a0cd583a8a927
CPMSP <cpetersonmsp@gmail.com>
2021-01-11 23:25:51 -0600

    Updates

---
+++ precache-manifest.cc78f29357a4219ef12b440e0d812393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a961762ca5d92d68afb03021e76ac94b",
+    "url": "/dadjokes/index.html"
+  },
+  {
+    "revision": "dc46fe685d286bd1409c",
+    "url": "/dadjokes/static/css/main.d2b0cc34.chunk.css"
+  },
+  {
+    "revision": "e47c49b50f1876cbf75b",
+    "url": "/dadjokes/static/js/2.140a1bc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dadjokes/static/js/2.140a1bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc46fe685d286bd1409c",
+    "url": "/dadjokes/static/js/main.5ef4190a.chunk.js"
+  },
+  {
+    "revision": "e42215d09e789f9cd78c",
+    "url": "/dadjokes/static/js/runtime-main.15419db5.js"
+  }
+]);
